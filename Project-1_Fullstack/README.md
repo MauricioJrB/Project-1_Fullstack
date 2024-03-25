69997e37173eb31b2871a159c8f3ce3a7363a365
Mauricio Junior <mjunior1205@gmail.com>
2024-03-25 20:24:10 -0300

    Deploy first project from Web Fullstack discipline

---
+++ Project-1_Fullstack/README.md
@@ -0,0 +1,3 @@
+Link para acesso: https://mauriciojrb.github.io/AS64A.2024_1-Web_Fullstack/Project-1_Fullstack/index.html
+
+![proposta_projeto1_2024_1_page-0001](https://github.com/MauricioJrB/AS64A.2024_1-Web_Fullstack/assets/127567339/cb80a3a5-fc82-4124-893b-845d3675105c)
---
+++ Project-1_Fullstack/script.js
@@ -0,0 +1,49 @@
+let ul = document.getElementById("ul");
+let animeLi = document.getElementById("anime");
+let personagemLi = document.getElementById("personagem");
+let citacaoLi = document.getElementById("citacao");
+let res = document.getElementById("res");
+
+const url = "https://animechan.xyz/api/random/character?name=";
+
+document.getElementById("ul").style.display = "none";
+
+let input = document.getElementById("input").value;
+
+const validationInput = (input) => {
+  return new Promise((resolve, reject) => {
+    if (input.trim() === "") {
+      document.getElementById("ul").style.display = "none";
+      res.innerHTML += "<p>Insira um nome no campo de busca!</p>";
+      reject("Empty field");
+    } else {
+      resolve(input);
+    }
+  });
+};
+
+document.getElementById("btn-Buscar").addEventListener("click", async () => {
+  input = document.getElementById("input").value;
+  try {
+    res.innerHTML = "";
+    animeLi.innerHTML = "";
+    personagemLi.innerHTML = "";
+    citacaoLi.innerHTML = "";
+
+    await validationInput(input);
+
+    const response = await fetch(url + input);
+    const data = await response.json();
+
+    if (data.anime === undefined && data.character === undefined) {
+      res.innerHTML += "<p>Personagem não encontrado!</p>";
+    } else {
+      document.getElementById("ul").style.display = "";
+      animeLi.innerHTML += `<p>Anime:<br>${data.anime}</p>`;
+      personagemLi.innerHTML += `<p>Personagem:<br>${data.character}</p>`;
+      citacaoLi.innerHTML += `<p>Citação:${data.quote}</p>`;
+    }
+  } catch (error) {
+    console.error("Erro ao solicitar dados da API:", error);
+  }
+});
